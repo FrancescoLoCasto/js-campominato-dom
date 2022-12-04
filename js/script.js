fb95f48b7daa5d9c00d7d6272d6c2a8492074f2c
MyUsername <my@email.com>
2022-12-04 16:39:14 +0100

    recode js post recap

--- js/script.js
+++ js/script.js
@@ -4,23 +4,7 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
 
-const startElement = document.getElementById('playButton');
 const boxElement = document.getElementById('largeBox');
+const startElement = document.getElementById('playButton');
 const sectionMainElement = document.getElementById('sectionMain');
-
-startElement.addEventListener('click', function(){
-   boxElement.innerHTML = ""
-   boxElement.classList.add('largeBox')
-   const grenadeList = []
-
-   let smallCube
-   for(let i = 1; i <= 100; i++) {
-      smallCube = createCube();
-      smallCube.innerHTML = i
-      boxElement.appendChild(smallCube)
-      grenadeList.push(smallCube)
-   }
-   
-
-
-})
+const value = document.getElementById('value');
